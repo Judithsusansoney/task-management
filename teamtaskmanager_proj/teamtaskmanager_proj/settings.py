62a29737e30ca7c711be66dc731b339773039f16
Judithsusansoney <judithsusansoney@gmail.com>
2026-05-04 00:03:16 +0530

    Configure PostgreSQL support for Railway

--- teamtaskmanager_proj/teamtaskmanager_proj/settings.py
+++ teamtaskmanager_proj/teamtaskmanager_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,10 +79,9 @@ WSGI_APPLICATION = 'teamtaskmanager_proj.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+    )
 }
 
 
